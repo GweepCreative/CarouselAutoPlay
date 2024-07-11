4ccb3b7ba5317036317c6584406d9b39dfb6df36
GweepCreative <gweepcreative@gmail.com>
2024-07-11 14:20:08 +0300

    autoplay

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { Button } from "@/components/ui/button";
 import {
   Carousel,
   CarouselContent,
@@ -8,9 +9,11 @@ import {
   type CarouselApi,
 } from "@/components/ui/carousel";
 import Autoplay from "embla-carousel-autoplay";
+import Link from "next/link";
 import { useEffect, useRef, useState } from "react";
 
 export default function Home() {
+
   const [api, setApi] = useState<CarouselApi>();
   const [current, setCurrent] = useState(0);
   const [count, setCount] = useState(0);
@@ -27,11 +30,15 @@ export default function Home() {
     });
   }, [api]);
 
-  const plugin = useRef(Autoplay({ delay: 2000, stopOnInteraction: true }));
+  const plugin = useRef(Autoplay({ delay: 2000, stopOnInteraction: false }));
+
+
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
       <div className="p-2">
-        <p className="font-bold text-xl">Shadcn/ui carousel autoplay example (with plugin)</p>
+        <p className="font-bold text-xl">
+          Shadcn/ui carousel autoplay example (with plugin)
+        </p>
       </div>
 
       <div className="bg-zinc-400 w-[38rem]">
@@ -56,10 +63,7 @@ export default function Home() {
               />
             </CarouselItem>
             <CarouselItem>
-              <img
-                src="https://i.redd.it/6uoazfklyo7b1.jpg"
-                alt=""
-              />
+              <img src="https://i.redd.it/6uoazfklyo7b1.jpg" alt="" />
             </CarouselItem>
           </CarouselContent>
           <CarouselPrevious />
@@ -68,6 +72,21 @@ export default function Home() {
       </div>
       <div className="py-2 text-center text-sm text-muted-foreground">
         Slide {current} of {count}
+      </div>
+      <div className="flex flex-col justify-center items-center gap-2 mt-10">
+        <Link
+          className="mx-2 w-full"
+          href={"https://github.com/GweepCreative/CarouselAutoPlay"}
+        >
+          <Button className="w-full">Source Code on Github</Button>
+        </Link>
+
+        <Link
+          className="mx-2 w-full"
+          href={"https://grkn.dev"}
+        >
+          <Button className="w-full">By GrknDev</Button>
+        </Link>
       </div>
     </main>
   );
